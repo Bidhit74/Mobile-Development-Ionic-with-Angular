0a17cfb2532c77bdda91b83a236e41de70dd2a01
Bidhit <bidhitkumar61@gmail.com>
2025-02-07 09:18:29 +0530

    add @capacitor/camera and @ionic/pwa-elements dependencies; update main.ts to define custom elements

--- MyFirstProjectWithStandalone/src/main.ts
+++ MyFirstProjectWithStandalone/src/main.ts
@@ -1,10 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+import { defineCustomElements as pwaCameraElements } from '@ionic/pwa-elements/loader';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
+pwaCameraElements(window);
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
