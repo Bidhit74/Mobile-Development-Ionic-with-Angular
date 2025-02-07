2c0832402ec253635e66ddd82c4a7fb01388263c
Bidhit <bidhitkumar61@gmail.com>
2025-02-07 09:07:10 +0530

    refactor item details page to display item name and description, and enhance navigation with back button

--- MyFirstProjectWithStandalone/src/app/home/item-details/item-details.page.ts
+++ MyFirstProjectWithStandalone/src/app/home/item-details/item-details.page.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonBackButton,
+  IonCard,
+  IonCardContent,
+} from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { GroceryService } from 'src/app/Services/grocery.service';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.page.html',
   styleUrls: ['./item-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCardContent,
+    IonCard,
+    IonBackButton,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class ItemDetailsPage implements OnInit {
-
-  constructor() { }
-
+  item: any;
+  private route = inject(ActivatedRoute);
+  private groceryList = inject(GroceryService);
   ngOnInit() {
+    this.getParam();
   }
-
+  getParam() {
+    const id = this.route.snapshot.paramMap.get('id');
+    const grocery = this.groceryList.getItems();
+    this.item = grocery.find((item) => item.id == id);
+  }
 }
